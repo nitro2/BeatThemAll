f717aa41f02df40fbb2835942e9315b0e34d7ef1
bananaback <votrongtin882003@gmail.com>
2022-12-11 02:55:16 +0700

    split file

---
+++ src/character/wizard.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _WIZARD_H_
+#define _WIZARD_H_
+
+#include "character.hpp"
+
+class Wizard : public Character
+{
+public:
+    Wizard();
+    ~Wizard();
+};
+
+
+#endif // !_WIZARD_H_
