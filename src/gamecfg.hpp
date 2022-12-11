596ad65a23b8fed99223842230ca5896ba016ea2
nitro2 <ngothanhnhannt@gmail.com>
2022-12-11 20:06:22 +0700

    Use config file

---
+++ src/gamecfg.hpp
@@ -0,0 +1,41 @@
+#ifndef _GAMECFG_HPP_
+#define _GAMECFG_HPP_
+
+// Common character config
+#define CFG_CHARACTER_HEALTH 100
+#define CFG_CHARACTER_WIDTH 240
+#define CFG_CHARACTER_HEIGHT 320
+
+// Skeleton
+#define CFG_SKELETON_IMG_SWITCH_TIME 0.5f
+#define CFG_SKELETON_IMG_ATTACK_PATH "assets/char/skeleton/image/skeleton_attack.png"
+#define CFG_SKELETON_IMG_ATTACK_FRAMES 18
+#define CFG_SKELETON_IMG_DEAD_PATH "assets/char/skeleton/image/skeleton_dead.png"
+#define CFG_SKELETON_IMG_DEAD_FRAMES 15
+#define CFG_SKELETON_IMG_HIT_PATH "assets/char/skeleton/image/skeleton_hit.png"
+#define CFG_SKELETON_IMG_HIT_FRAMES 8
+#define CFG_SKELETON_IMG_IDLE_PATH "assets/char/skeleton/image/skeleton_idle.png"
+#define CFG_SKELETON_IMG_IDLE_FRAMES 11
+#define CFG_SKELETON_IMG_WALK_PATH "assets/char/skeleton/image/skeleton_walk.png"
+#define CFG_SKELETON_IMG_WALK_FRAMES 13
+#define CFG_SKELETON_STAT_ATTACK 10
+#define CFG_SKELETON_STAT_DEFEND 5
+#define CFG_SKELETON_STAT_HEALTH CFG_CHARACTER_HEALTH
+
+// Warrior
+#define CFG_WARRIOR_IMG_SWITCH_TIME 0.08f
+#define CFG_WARRIOR_IMG_ATTACK_PATH "assets/char/warrior/image/warrior_idle.png"
+#define CFG_WARRIOR_IMG_ATTACK_FRAMES
+#define CFG_WARRIOR_IMG_DEAD_PATH "assets/char/warrior/image/warrior_dead.png"
+#define CFG_WARRIOR_IMG_DEAD_FRAMES
+#define CFG_WARRIOR_IMG_HIT_PATH "assets/char/warrior/image/warrior_hit.png"
+#define CFG_WARRIOR_IMG_HIT_FRAMES
+#define CFG_WARRIOR_IMG_IDLE_PATH "assets/char/warrior/image/warrior_idle.png"
+#define CFG_WARRIOR_IMG_IDLE_FRAMES
+#define CFG_WARRIOR_IMG_WALK_PATH "assets/char/warrior/image/warrior_walk.png"
+#define CFG_WARRIOR_IMG_WALK_FRAMES
+#define CFG_WARRIOR_STAT_ATTACK 10
+#define CFG_WARRIOR_STAT_DEFEND 5
+#define CFG_WARRIOR_STAT_HEALTH CFG_CHARACTER_HEALTH
+
+#endif // _GAMECFG_HPP_
