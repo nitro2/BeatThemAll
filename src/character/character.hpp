803f7ba4c35a29bd9ee3ffee4bd7aa1c64d0224a
nitro2 <ngothanhnhannt@gmail.com>
2022-12-19 17:10:05 +0700

    Move box and grid to debug folder

--- src/character/character.hpp
+++ src/character/character.hpp
@@ -4,7 +4,7 @@
 #include "SFML/Graphics.hpp"
 #include "gameobject.hpp"
 #include "animation.hpp"
-#include "debug.hpp"
+#include "box.hpp" // Debug box
 class Character : public GameObject
 {
 public:
